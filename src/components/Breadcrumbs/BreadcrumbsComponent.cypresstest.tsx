8eba4b5dd5eb82f76e121dcede3c10b00ec0420b
Chunguang Yang <chungguangy@avaya.com>
2021-08-31 15:06:45 -0400

    update cypress component test

--- src/components/Breadcrumbs/BreadcrumbsComponent.cypresstest.tsx
+++ src/components/Breadcrumbs/BreadcrumbsComponent.cypresstest.tsx
@@ -3,8 +3,8 @@ import { Breadcrumbs } from "./BreadcrumbsComponent";
 
 describe("Breadcrumbs component", () => {
   describe("Current page only", () => {
-    it("renders description", () => {
-      const currentPageLink = { href: "root", text: "root" };
+    it("renders ok", () => {
+      const currentPageLink = { href: "#root", text: "root" };
       const description = "Breadcrumb Example page description";
 
       const props = {
@@ -14,6 +14,11 @@ describe("Breadcrumbs component", () => {
       const rootElement = "[data-testid='Breadcrumbs-root']";
       mount(<Breadcrumbs {...props} />);
       cy.get(rootElement).should("contain.text", description);
+      cy.get(".neo-breadcrumbs__link--current")
+        .should("have.class", "neo-breadcrumbs__link")
+        .find("a")
+        .should("contain", "root")
+        .should("have.attr", "href", "#root");
     });
   });
 });
