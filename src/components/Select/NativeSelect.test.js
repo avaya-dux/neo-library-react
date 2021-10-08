83f2555b807646f842f5c24830b7c082aa38ab19
Lucas Matos <lucas@bertonisolutions.com>
2021-10-08 10:17:18 -0600

    update test

--- src/components/Select/NativeSelect.test.js
+++ src/components/Select/NativeSelect.test.js
@@ -1,13 +1,33 @@
+import { composeStories } from "@storybook/testing-react";
+import * as NativeSelectStories from "./NativeSelect.stories";
 import { render } from "@testing-library/react";
 import { axe } from "jest-axe";
 
-import { DefaultNativeSelect } from "./NativeSelect.stories";
+const { DefaultNativeSelect, NativeSelectExample } =
+  composeStories(NativeSelectStories);
 
 describe("NativeSelect: ", () => {
   describe("Default", () => {
     let renderResult;
     beforeEach(() => {
       renderResult = render(<DefaultNativeSelect />);
+    });
+    it("should render ok", () => {
+      const { container } = renderResult;
+      expect(container).not.toBe(null);
+    });
+
+    it("passes basic axe compliance", async () => {
+      const { container } = renderResult;
+      const results = await axe(container);
+      expect(results).toHaveNoViolations();
+    });
+  });
+
+  describe("Native Select Example", () => {
+    let renderResult;
+    beforeEach(() => {
+      renderResult = render(<NativeSelectExample />);
     });
     it("should render ok", () => {
       const { container } = renderResult;
