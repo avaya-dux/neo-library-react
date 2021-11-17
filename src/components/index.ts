ebd21f0e47e4b495b1fccacbc843327e506f1e05
Enrique Prado <enriquep@avaya.com>
2021-11-17 08:32:57 -0800

    NEO-729: InfoModal Component (#96)
    * InfoModal code complete with tests

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,6 +9,7 @@ export * from "./IconButton";
 export * from "./Image";
 export * from "./List";
 export * from "./ListItem";
+export * from "./Modal";
 export * from "./NeoInputWrapper";
 export * from "./NoContent";
 export * from "./Pagination";
