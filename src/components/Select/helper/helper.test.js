bed824d5a3bb020257f56c7acfa692e85aaf8d97
Lucas Matos <lucas@bertonisolutions.com>
2021-11-23 16:02:08 -0700

    update test

--- src/components/Select/helper/helper.test.js
+++ src/components/Select/helper/helper.test.js
@@ -9,31 +9,31 @@ describe("Helper test ", () => {
   describe("getOption", () => {
     it("given a list of states without a query will returns the default selected", () => {
       expect(getOption(listOfStates)).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "defaultSelected": true,
-          "disabled": false,
-          "label": "--Please choose an option--",
-          "placeholder": true,
-          "value": "0",
-        },
-      ]
-    `);
+        Array [
+          Object {
+            "defaultSelected": true,
+            "isDisabled": false,
+            "isPlaceholder": true,
+            "label": "--Please choose an option--",
+            "value": "0",
+          },
+        ]
+      `);
     });
 
     it("given a list of states with the query (AL, UT) will returns Alabama and Utah", () => {
       expect(getOption(listOfStates, ["AL", "UT"])).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "label": "Alabama",
-          "value": "AL",
-        },
-        Object {
-          "label": "Utah",
-          "value": "UT",
-        },
-      ]
-    `);
+              Array [
+                Object {
+                  "label": "Alabama",
+                  "value": "AL",
+                },
+                Object {
+                  "label": "Utah",
+                  "value": "UT",
+                },
+              ]
+          `);
     });
   });
 
@@ -41,55 +41,55 @@ describe("Helper test ", () => {
     it("given a ErrorMessagesDemo and HelperMessagesDemo, should do return the list of errors", () => {
       expect(displayErrorOrHelper(errorMessagesDemo, helperMessagesDemo))
         .toMatchInlineSnapshot(`
-      Array [
-        <div
-          className="neo-input-hint"
-        >
-          Error 1: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
-        </div>,
-        <div
-          className="neo-input-hint"
-        >
-          Error 2: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
-        </div>,
-      ]
-    `);
+              Array [
+                <div
+                  className="neo-input-hint"
+                >
+                  Error 1: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
+                </div>,
+                <div
+                  className="neo-input-hint"
+                >
+                  Error 2: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
+                </div>,
+              ]
+          `);
     });
 
     it("given only ErrorMessagesDemo, should do return the list of errors", () => {
       expect(displayErrorOrHelper(errorMessagesDemo, undefined))
         .toMatchInlineSnapshot(`
-      Array [
-        <div
-          className="neo-input-hint"
-        >
-          Error 1: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
-        </div>,
-        <div
-          className="neo-input-hint"
-        >
-          Error 2: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
-        </div>,
-      ]
-    `);
+              Array [
+                <div
+                  className="neo-input-hint"
+                >
+                  Error 1: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
+                </div>,
+                <div
+                  className="neo-input-hint"
+                >
+                  Error 2: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
+                </div>,
+              ]
+          `);
     });
 
     it("given only HelperMessagesDemo, should do return the list of help messages", () => {
       expect(displayErrorOrHelper(undefined, helperMessagesDemo))
         .toMatchInlineSnapshot(`
-      Array [
-        <div
-          className="neo-input-hint"
-        >
-          Helper 1: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
-        </div>,
-        <div
-          className="neo-input-hint"
-        >
-          helper 2: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
-        </div>,
-      ]
-    `);
+              Array [
+                <div
+                  className="neo-input-hint"
+                >
+                  Helper 1: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
+                </div>,
+                <div
+                  className="neo-input-hint"
+                >
+                  helper 2: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
+                </div>,
+              ]
+          `);
     });
   });
 });
