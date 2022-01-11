8d637d515ed3d8d9668c2566a426cf4956ca6583
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2022-01-11 13:29:03 -0800

    adding additional stories for `SelectNative`, upgrading unit test code coverage to 100% (#33)
    * notes to self
    
    * add form example story and unit test for it
    
    * add "loading" story, and unit test for it
    
    * update code coverage badges
    
    Co-authored-by: Joe S <jsebast@avaya.com>

--- src/components/SelectNative/SelectNative.stories.tsx
+++ src/components/SelectNative/SelectNative.stories.tsx
@@ -1,4 +1,7 @@
 import { Meta, Story } from "@storybook/react/types-6-0";
+import { useCallback, useEffect, useState } from "react";
+
+import { Button, Form } from "components";
 
 import { SelectNative, SelectNativeProps } from ".";
 
@@ -21,4 +24,98 @@ Templated.args = {
   label: "Choose a car:",
   helperText: "Example of helper text",
   errorList: ["Error One", "Error Two"],
+};
+
+export const FormSubmission = () => {
+  const helperTextExample = "example of helper text";
+  const [helperText, setHelperText] = useState(helperTextExample);
+  const [errorList, setErrorList] = useState<string[]>([]);
+  const [chosenCar, setChosenCar] = useState("");
+
+  useEffect(() => {
+    if (chosenCar !== "") {
+      setHelperText(helperTextExample);
+      setErrorList([]);
+    }
+  }, [chosenCar]);
+
+  return (
+    <Form
+      inline
+      onSubmit={(e) => {
+        e.preventDefault();
+        alert(`you successfully submitted: ${chosenCar}`);
+      }}
+    >
+      <SelectNative
+        defaultValue=""
+        errorList={errorList}
+        helperText={helperText}
+        label="Choose a car:"
+        onChange={(e) => setChosenCar((e.target as any).value)}
+        required
+      >
+        <option value="" disabled hidden>
+          select an option
+        </option>
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </SelectNative>
+
+      <Button
+        style={{ marginRight: 10 }}
+        type="submit"
+        onClick={() => {
+          if (chosenCar === "") {
+            setHelperText("");
+            setErrorList(["you must select an option"]);
+          }
+        }}
+      >
+        Submit
+      </Button>
+
+      <Button type="reset" onClick={() => setChosenCar("")}>
+        Reset
+      </Button>
+    </Form>
+  );
+};
+
+export const LoadOptions = () => {
+  const [loading, setLoading] = useState(false);
+  const [options, setOptions] = useState<string[]>([]);
+
+  const fakeLoad = useCallback(() => {
+    setLoading(true);
+    setOptions([]);
+    setTimeout(() => {
+      setOptions(["volvo", "saab", "mercedes", "audi"]);
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  useEffect(fakeLoad, [fakeLoad]);
+
+  return (
+    <>
+      <SelectNative
+        label="Choose a car:"
+        loading={loading}
+        onChange={(e) => setOptions([(e.target as any).value])}
+      >
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </SelectNative>
+
+      <Button disabled={loading} onClick={fakeLoad}>
+        Fake load options
+      </Button>
+    </>
+  );
 };
