8082aff0e1e117f28081232c426f607e9b9c186b
Joe S <jsebast@avaya.com>
2022-01-19 15:28:25 -0800

    move `TableFilter.tsx` closer to usable

--- src/components/Table/TableComponents/TableFilter.tsx
+++ src/components/Table/TableComponents/TableFilter.tsx
@@ -1,31 +1,53 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { TableInstance } from "react-table";
 
 import { Button } from "components/Button";
 import { IconButton } from "components/IconButton";
 import { Sheet } from "components/Sheet";
 
+import { ITableFilterTranslations } from "../types";
+import { translations as defaultTranslations } from "../default-data";
+import { Checkbox, NeoInputWrapper } from "components";
+
 type TableFilterProps<T extends Record<string, any>> = {
-  filter?: string;
-  title?: string;
+  translations: ITableFilterTranslations;
   instance: TableInstance<T>;
 };
 
 export const TableFilter = <T extends Record<string, any>>({
-  filter = "Filter",
-  title = "Filter By",
+  translations,
   instance,
 }: TableFilterProps<T>) => {
+  const {
+    state: { filters, globalFilter },
+    // setGlobalFilter,
+  } = instance;
+  console.log("filters", filters);
+  console.log("globalFilter", globalFilter);
+
+  const close =
+    translations.close || (defaultTranslations.toolbar.close as string);
+  const clear = translations.clear || defaultTranslations.toolbar.clear;
+  const filter = translations.filter || defaultTranslations.toolbar.filter;
+  const filterBy =
+    translations.filterBy || defaultTranslations.toolbar.filterBy;
+
   const [filtersVisible, setFiltersVisible] = useState(false);
-  const handleFilter = useCallback(() => {
+  const toggleFilter = () => {
     setFiltersVisible(!filtersVisible);
-  }, []);
+    // setSearches(event.target.value);
+  };
 
   const buttons = [
-    <Button variant="secondary" onClick={handleFilter}>
-      Cancel
-    </Button>,
-    <Button onClick={handleFilter}>Apply</Button>,
+    <IconButton
+      aria-label={close}
+      icon="close"
+      shape="circle"
+      style={{ color: "black" }}
+      variant="tertiary"
+      onClick={toggleFilter}
+      key="table-filter-close-icon-button"
+    />,
   ];
 
   return (
@@ -34,133 +56,31 @@ export const TableFilter = <T extends Record<string, any>>({
         aria-label={filter || "Filter"}
         icon="filter"
         shape="square"
-        onClick={handleFilter}
+        onClick={toggleFilter}
       />
 
       {filtersVisible && (
         <Sheet
           className="neo-table__filters--sheet"
-          title={title}
+          style={{ width: 200 }}
+          title={filterBy}
           buttons={buttons}
         >
-          <form className="neo-form">
-            <div className="neo-form-control">
-              <div className="neo-input-group">
-                <label
-                  htmlFor="checkGroupExample"
-                  aria-label="Addon on the right"
-                >
-                  Category
-                </label>
-                <input
-                  className="neo-check"
-                  type="checkbox"
-                  id="checkbox1"
-                  value="Checkbox 1"
-                  aria-checked="false"
-                />
-                <label htmlFor="checkbox1">Option</label>
-                <input
-                  className="neo-check"
-                  type="checkbox"
-                  id="checkbox2"
-                  value="Checkbox 2"
-                  aria-checked="false"
-                />
-                <label htmlFor="checkbox2">Option</label>
-                <input
-                  className="neo-check"
-                  type="checkbox"
-                  id="checkbox3"
-                  value="Checkbox 3"
-                  aria-checked="false"
-                />
-                <label htmlFor="checkbox3">Option</label>
-                <input
-                  className="neo-check"
-                  type="checkbox"
-                  id="checkbox4"
-                  value="Checkbox 4"
-                  aria-checked="false"
-                />
-                <label className="checked" htmlFor="checkbox4">
-                  Option
-                </label>
-                <input
-                  className="neo-check"
-                  type="checkbox"
-                  id="checkbox5"
-                  value="Checkbox 5"
-                  aria-checked="false"
-                />
-                <label className="checked" htmlFor="checkbox5">
-                  Option
-                </label>
-              </div>
-            </div>
-            <div className="neo-form-control">
-              <div className="neo-input-group">
-                <label
-                  htmlFor="checkGroupExample"
-                  aria-label="Addon on the right"
-                >
-                  Category
-                </label>
-                <input
-                  className="neo-check"
-                  type="checkbox"
-                  id="checkbox6"
-                  value="Checkbox 6"
-                  aria-checked="false"
-                />
-                <label htmlFor="checkbox6">Option</label>
-                <input
-                  className="neo-check"
-                  type="checkbox"
-                  id="checkbox7"
-                  value="Checkbox 7"
-                  aria-checked="false"
-                />
-                <label htmlFor="checkbox7">Option</label>
-                <input
-                  className="neo-check"
-                  type="checkbox"
-                  id="checkbox8"
-                  value="Checkbox 8"
-                  aria-checked="false"
-                />
-                <label htmlFor="checkbox8">Option</label>
-                <input
-                  className="neo-check"
-                  type="checkbox"
-                  id="checkbox9"
-                  value="Checkbox 9"
-                  aria-checked="false"
-                />
-                <label className="checked" htmlFor="checkbox  9">
-                  Option
-                </label>
-                <input
-                  className="neo-check"
-                  type="checkbox"
-                  id="checkbox10"
-                  value="Checkbox 10"
-                  aria-checked="false"
-                />
-                <label className="checked" htmlFor="checkbox10">
-                  Option
-                </label>
-              </div>
-            </div>
-          </form>
+          <section>
+            <NeoInputWrapper>
+              <Checkbox label="check one" value="1" onChange={() => {}} />
+              <Checkbox label="check two" value="2" onChange={() => {}} />
+            </NeoInputWrapper>
+          </section>
 
           <div className="neo-table__filters--sheet__footer">
             <Button
-              className="neo-btn--wide neo-btn-tertiary--alert"
-              onClick={handleFilter}
+              onClick={toggleFilter}
+              size="wide"
+              status="alert"
               variant="tertiary"
             >
-              Clear Filters
+              {clear}
             </Button>
           </div>
         </Sheet>
