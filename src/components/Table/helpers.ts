3572b8e952145fecfaadc8c50e61f322477f3b90
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2022-01-24 04:53:13 -0800

    minor cleanup (#44)
    * minor cleanup
    
    remove unnecessary interface prop from `IconChipProps`
    
    fix typing in a story for `SelectNative`
    
    added auto-generated doc for two helper methods in `Table`
    
    * revert IconChip change
    
    Co-authored-by: Joe S <jsebast@avaya.com>

--- src/components/Table/helpers.ts
+++ src/components/Table/helpers.ts
@@ -1,5 +1,11 @@
 import { AriaAttributes } from "react";
 
+/**
+ * If the table is sorted, return the aria-sort value.
+ * @param {boolean} isSorted - boolean
+ * @param {"descending" | "ascending"} [sortedDir] - The direction of the sort.
+ * @returns "none"
+ */
 export const calculateAriaSortValue = (
   isSorted: boolean,
   sortedDir?: "descending" | "ascending"
@@ -17,6 +23,11 @@ export const calculateAriaSortValue = (
   return result;
 };
 
+/**
+ * Given an array of row ids, return an object with the same keys and values.
+ * @param rowIds - string[]
+ * @returns An object with the rowIds as keys and true as values.
+ */
 export const convertRowIdsArrayToObject = (rowIds: string[]) => {
   const result: Record<string, boolean> = {};
   rowIds.forEach((rowId) => {
