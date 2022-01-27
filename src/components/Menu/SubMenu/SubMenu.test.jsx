bb4d11342b9326538f77cb84dcceb01da1c79a55
Joe S <jsebast@avaya.com>
2022-01-27 14:37:37 -0800

    fix submenu tests

--- src/components/Menu/SubMenu/SubMenu.test.jsx
+++ src/components/Menu/SubMenu/SubMenu.test.jsx
@@ -1,17 +1,14 @@
-import { composeStories } from "@storybook/testing-react";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { axe } from "jest-axe";
+import { render } from "@testing-library/react";
 import log from "loglevel";
-import { MenuItem } from "./MenuItem";
-import { MenuSeparator } from "./MenuSeparator";
+
+import { MenuItem } from "../MenuItem";
+import { MenuSeparator } from "../MenuSeparator";
 import {
   addIdToChildren,
   buildMenuIndexes,
   layoutChildren,
   SubMenu,
 } from "./SubMenu";
-import * as SubMenuStories from "./SubMenu.stories";
 
 const subMenuLogger = log.getLogger("submenu");
 subMenuLogger.disableAll();
@@ -20,82 +17,53 @@ keyboardLogger.disableAll();
 const mouseLogger = log.getLogger("menu-mouse-event-handler");
 mouseLogger.disableAll();
 
-const { WithSeparator } = composeStories(SubMenuStories);
-describe("Storybook tests", () => {
-  describe("WithSeparator", () => {
-    let renderResult;
-    beforeEach(() => {
-      renderResult = render(<WithSeparator action="ENTER_SUB_MENU" />);
-    });
-    it("should render ok", () => {
-      const { container } = renderResult;
-      expect(container).toBeDefined();
-    });
-
-    it("passes basic axe compliance", async () => {
-      const { container } = renderResult;
-      const results = await axe(container);
-      expect(results).toHaveNoViolations();
-    });
-
-    it("should advance cursor to next menu item with ArrowDown", async () => {
-      const { getByText } = renderResult;
-      const button = getByText("Item1");
-      expect(button).toHaveFocus();
-
-      // button arrowdown will open menu and move focus to first menu item
-      userEvent.keyboard("{ArrowDown}");
-      const item2 = await screen.queryByText("Item2");
-      expect(item2).toHaveAttribute("tabindex", "0");
-
-      const item3 = screen.getByText("Item3");
-      userEvent.hover(item3);
-      userEvent.hover(item3);
-
-      const item3Updated = await screen.queryByText("Item3");
-      expect(item3Updated).toHaveAttribute("tabindex", "0");
-    });
-  });
-});
-
 describe("SubMenu helper methods", () => {
   describe(addIdToChildren, () => {
     it("should do nothing when child is not menu item or sub menu", () => {
       const children = [<MenuSeparator />];
       expect(addIdToChildren(children)).toEqual(children);
     });
+
     it("should add id when menu item does not have id", () => {
-      const menuItem = <MenuItem text="View" />;
+      const menuItem = <MenuItem>View</MenuItem>;
       expect(addIdToChildren([menuItem])[0].props.id).not.toBeNull();
     });
+
     it("should keep the id when menu item has id", () => {
-      const menuItem = <MenuItem id="id" text="View" />;
+      const menuItem = <MenuItem id="id">View</MenuItem>;
       expect(addIdToChildren([menuItem])[0].props.id).toBe("id");
     });
+
     it("should add id to button when submenu button does not have id", () => {
-      const subMenu = <SubMenu menuRootElement={<MenuItem text="File" />} />;
+      const subMenu = <SubMenu menuRootElement={<MenuItem>File</MenuItem>} />;
       expect(
-        addIdToChildren([subMenu])[0].props.button.props.id
+        addIdToChildren([subMenu])[0].props.menuRootElement.props.id
       ).not.toBeNull();
     });
+
     it("should keep the button id when submenu button has id", () => {
       const subMenu = (
-        <SubMenu menuRootElement={<MenuItem id="id" text="File" />} />
+        <SubMenu menuRootElement={<MenuItem id="id">File</MenuItem>} />
       );
-      expect(addIdToChildren([subMenu])[0].props.button.props.id).toBe("id");
+      expect(addIdToChildren([subMenu])[0].props.menuRootElement.props.id).toBe(
+        "id"
+      );
     });
   });
+
   describe(buildMenuIndexes, () => {
     it("should return empty array with empty children", () => {
       const children = [];
       expect(buildMenuIndexes(children)).toEqual([]);
     });
+
     it("should return empty array with just MenuSeparator", () => {
       const children = [<MenuSeparator />];
       expect(buildMenuIndexes(children)).toEqual([]);
     });
+
     it("should return correct result with one MenuItem", () => {
-      const children = [<MenuItem text="View" />];
+      const children = [<MenuItem>View</MenuItem>];
       expect(buildMenuIndexes(children)).toMatchInlineSnapshot(`
       Array [
         Object {
@@ -105,15 +73,16 @@ describe("SubMenu helper methods", () => {
       ]
     `);
     });
+
     it("should return correct result with one SubMenu", () => {
       const children = [
         <SubMenu
           id="sub"
-          menuRootElement={<MenuItem text="File" id="button" />}
+          menuRootElement={<MenuItem id="button">File</MenuItem>}
         >
-          <MenuItem text="View" />
-          <MenuItem text="Edit" />
-          <MenuItem text="Delete" />
+          <MenuItem>View</MenuItem>
+          <MenuItem>Edit</MenuItem>
+          <MenuItem>Delete</MenuItem>
         </SubMenu>,
       ];
       expect(buildMenuIndexes(children)).toMatchInlineSnapshot(`
@@ -126,17 +95,18 @@ describe("SubMenu helper methods", () => {
       ]
     `);
     });
+
     it("should return correct result with one MenuItem and one SubMenu", () => {
       const children = [
         <SubMenu
           id="sub"
-          menuRootElement={<MenuItem text="File" id="button" />}
+          menuRootElement={<MenuItem id="button">File</MenuItem>}
         >
-          <MenuItem text="View" />
-          <MenuItem text="Edit" />
-          <MenuItem text="Delete" />
+          <MenuItem>View</MenuItem>
+          <MenuItem>Edit</MenuItem>
+          <MenuItem>Delete</MenuItem>
         </SubMenu>,
-        <MenuItem id="help" text="Help" />,
+        <MenuItem id="help">Help</MenuItem>,
       ];
       expect(buildMenuIndexes(children)).toMatchInlineSnapshot(`
       Array [
@@ -163,13 +133,11 @@ describe("SubMenu helper methods", () => {
       handleMenuMouseMove = jest.fn();
       handleMenuBlur = jest.fn();
     });
+
     it("should render active menu item correctly", () => {
       const menuIndexes = [{ index: 1 }];
       const cursor = 0;
-      const children = [
-        <MenuSeparator />,
-        <MenuItem id="1" text="View" href="fixme" />,
-      ];
+      const children = [<MenuSeparator />, <MenuItem id="1">View</MenuItem>];
       const result = layoutChildren(
         children,
         handleMenuKeyDown,
@@ -183,37 +151,34 @@ describe("SubMenu helper methods", () => {
       const { getByRole } = render(result);
       const menuItem = getByRole("menuitem");
       expect(menuItem).toMatchInlineSnapshot(`
-      <a
+      <div
         class="neo-dropdown__link neo-dropdown__link-active"
-        href="fixme"
         id="1"
         role="menuitem"
         tabindex="0"
       >
         View
-      </a>
+      </div>
     `);
     });
+
     it("should render active sub menu correctly", () => {
       const menuIndexes = [{ index: 0 }, { index: 1, length: 2 }];
       const cursor = 1;
       const testId = "submenu-test-id";
       const children = [
-        <MenuItem id="1" text="View" href="fixme" />,
+        <MenuItem id="1">View</MenuItem>,
         <SubMenu
           id="2"
           menuRootElement={
-            <MenuItem
-              id="20"
-              text="SubMenu"
-              data-testid={testId}
-              href="fixme"
-            ></MenuItem>
+            <MenuItem id="20" data-testid={testId}>
+              SubMenu
+            </MenuItem>
           }
         >
-          <MenuItem id="21" text="SubMenu-1" href="fixme" />
-          <MenuItem id="22" text="SubMenu-2" href="fixme" />
-          <MenuItem id="23" text="SubMenu-3" href="fixme" />
+          <MenuItem id="21">SubMenu-1</MenuItem>
+          <MenuItem id="22">SubMenu-2</MenuItem>
+          <MenuItem id="23">SubMenu-3</MenuItem>
         </SubMenu>,
       ];
       const result = layoutChildren(
@@ -229,16 +194,15 @@ describe("SubMenu helper methods", () => {
       const { getByTestId, container } = render(result);
       const subMenu = getByTestId(testId);
       expect(subMenu).toMatchInlineSnapshot(`
-      <a
+      <div
         class="neo-dropdown__link neo-dropdown__link-active"
         data-testid="submenu-test-id"
-        href="fixme"
         id="20"
         role="menuitem"
         tabindex="0"
       >
         SubMenu
-      </a>
+      </div>
     `);
       expect(container).toMatchInlineSnapshot(`
       <div>
@@ -247,61 +211,56 @@ describe("SubMenu helper methods", () => {
           role="menu"
           tabindex="-1"
         >
-          <a
+          <div
             class="neo-dropdown__link"
-            href="fixme"
             id="1"
             role="menuitem"
             tabindex="-1"
           >
             View
-          </a>
+          </div>
           <div
             class="neo-dropdown__item neo-dropdown--active"
             id="2"
           >
-            <a
+            <div
               class="neo-dropdown__link neo-dropdown__link-active"
               data-testid="submenu-test-id"
-              href="fixme"
               id="20"
               role="menuitem"
               tabindex="0"
             >
               SubMenu
-            </a>
+            </div>
             <div
               class="neo-dropdown__content"
               role="menu"
               tabindex="-1"
             >
-              <a
+              <div
                 class="neo-dropdown__link neo-dropdown__link-active"
-                href="fixme"
                 id="21"
                 role="menuitem"
                 tabindex="0"
               >
                 SubMenu-1
-              </a>
-              <a
+              </div>
+              <div
                 class="neo-dropdown__link"
-                href="fixme"
                 id="22"
                 role="menuitem"
                 tabindex="-1"
               >
                 SubMenu-2
-              </a>
-              <a
+              </div>
+              <div
                 class="neo-dropdown__link"
-                href="fixme"
                 id="23"
                 role="menuitem"
                 tabindex="-1"
               >
                 SubMenu-3
-              </a>
+              </div>
             </div>
           </div>
         </div>
@@ -314,7 +273,9 @@ describe("SubMenu helper methods", () => {
       const cursor = 2;
       const children = [
         <MenuSeparator />,
-        <MenuItem id="1" text="View" href="fixme" data-testid="inactive" />,
+        <MenuItem id="1" data-testid="inactive">
+          View
+        </MenuItem>,
       ];
       const result = layoutChildren(
         children,
@@ -329,38 +290,35 @@ describe("SubMenu helper methods", () => {
       const { getByTestId } = render(result);
       const menuItem = getByTestId("inactive");
       expect(menuItem).toMatchInlineSnapshot(`
-      <a
+      <div
         class="neo-dropdown__link"
         data-testid="inactive"
-        href="fixme"
         id="1"
         role="menuitem"
         tabindex="-1"
       >
         View
-      </a>
+      </div>
     `);
     });
+
     it("should render inactive sub menu correctly", () => {
       const menuIndexes = [{ index: 0 }, { index: 1, length: 2 }];
       const cursor = 0;
       const testId = "submenu-test-id";
       const children = [
-        <MenuItem id="1" text="View" href="fixme" />,
+        <MenuItem id="1">View</MenuItem>,
         <SubMenu
           id="2"
           menuRootElement={
-            <MenuItem
-              id="20"
-              text="SubMenu"
-              data-testid={testId}
-              href="fixme"
-            ></MenuItem>
+            <MenuItem id="20" data-testid={testId}>
+              SubMenu
+            </MenuItem>
           }
         >
-          <MenuItem id="21" text="SubMenu-1" href="fixme" />
-          <MenuItem id="22" text="SubMenu-2" href="fixme" />
-          <MenuItem id="23" text="SubMenu-3" href="fixme" />
+          <MenuItem id="21">SubMenu-1</MenuItem>
+          <MenuItem id="22">SubMenu-2</MenuItem>
+          <MenuItem id="23">SubMenu-3</MenuItem>
         </SubMenu>,
       ];
       const result = layoutChildren(
@@ -376,24 +334,24 @@ describe("SubMenu helper methods", () => {
       const { getByTestId } = render(result);
       const subMenu = getByTestId(testId);
       expect(subMenu).toMatchInlineSnapshot(`
-      <a
+      <div
         class="neo-dropdown__link"
         data-testid="submenu-test-id"
-        href="fixme"
         id="20"
         role="menuitem"
         tabindex="-1"
       >
         SubMenu
-      </a>
+      </div>
     `);
     });
+
     it("should render menu separator correctly", () => {
       const menuIndexes = [{ index: 1 }];
       const cursor = 0;
       const children = [
         <MenuSeparator data-testid="separator" />,
-        <MenuItem id="1" text="View" href="fixme" />,
+        <MenuItem id="1">View</MenuItem>,
       ];
       const result = layoutChildren(
         children,
