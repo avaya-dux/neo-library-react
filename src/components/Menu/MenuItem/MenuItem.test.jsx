75b3de245b61b242d1fdc7f86b4e9958e661a3e3
Joe S <jsebast@avaya.com>
2022-01-28 10:22:05 -0800

    NEO-799 // update Menu to take children
    and it's related components

--- src/components/Menu/MenuItem/MenuItem.test.jsx
+++ src/components/Menu/MenuItem/MenuItem.test.jsx
@@ -2,17 +2,19 @@ import { composeStories } from "@storybook/testing-react";
 import { render } from "@testing-library/react";
 import { axe } from "jest-axe";
 import log from "loglevel";
+
 import * as MenuItemStories from "./MenuItem.stories";
 
 log.disableAll();
 
-const { Item } = composeStories(MenuItemStories);
+const { Default } = composeStories(MenuItemStories);
 
 describe("Storybook tests", () => {
   let renderResult;
   beforeEach(() => {
-    renderResult = render(<Item />);
+    renderResult = render(<Default />);
   });
+
   it("should render ok", () => {
     const { container } = renderResult;
     expect(container).toBeDefined();
