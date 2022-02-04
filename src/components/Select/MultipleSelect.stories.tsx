b914a75bbbbaad7017367aeb9277c41ab28cdc8c
Mo Razian <mrazian@avaya.com>
2022-02-04 17:21:57 -0500

    Removed unnecessary Story import in stories file

--- src/components/Select/MultipleSelect.stories.tsx
+++ src/components/Select/MultipleSelect.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, Story } from "@storybook/react/types-6-0";
+import { Meta } from "@storybook/react/types-6-0";
 
 import { MultipleSelect, MultipleSelectOption } from "./MultipleSelect";
 import { MultipleSelectProps } from "./SelectTypes";
