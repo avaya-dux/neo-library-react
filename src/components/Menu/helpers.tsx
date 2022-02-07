53897ac5d2436da171346c9dbde19b7f273d08d8
Joe S <jsebast@avaya.com>
2022-02-07 10:41:29 -0800

    move helper methods from `SubMenu` to `helpers.tsx`

---
+++ src/components/Menu/helpers.tsx
@@ -0,0 +1,149 @@
+import log from "loglevel";
+import {
+  Children,
+  cloneElement,
+  FocusEventHandler,
+  Fragment,
+  isValidElement,
+  KeyboardEventHandler,
+  MouseEventHandler,
+  ReactElement,
+} from "react";
+
+import { genId } from "utils";
+
+import { MenuItem } from "./MenuItem";
+import {
+  ActionType,
+  MenuIndexesType,
+  MenuItemProps,
+  MenuProps,
+  SubMenuProps,
+} from "./MenuTypes";
+import { SubMenu } from "./SubMenu";
+
+const logger = log.getLogger("menu-helpers");
+logger.disableAll();
+
+export const addIdToChildren = (children: MenuProps["children"]) => {
+  return children.map((child) => {
+    if (child.type.toString() === MenuItem.toString()) {
+      const childId = child.props.id || genId();
+      return cloneElement(child, { id: childId });
+    } else if (child.type.toString() === SubMenu.toString()) {
+      const buttonElement = (child.props as SubMenuProps).menuRootElement;
+      const buttonElementId = buttonElement.props.id || genId();
+      const cloneButton = cloneElement(buttonElement, {
+        id: buttonElementId,
+      });
+      return cloneElement(child as ReactElement<SubMenuProps>, {
+        menuRootElement: cloneButton,
+      });
+    } else {
+      return child;
+    }
+  });
+};
+
+export const layoutChildren = (
+  children: MenuProps["children"],
+  handleMenuKeyDown: KeyboardEventHandler<HTMLDivElement>,
+  handleMenuMouseMove: MouseEventHandler,
+  handleMenuBlur: FocusEventHandler,
+  menuIndexes: MenuIndexesType,
+  cursor: number,
+  cursorAction: ActionType,
+  enterCounter: number
+) => {
+  return (
+    <div
+      className="neo-dropdown__content"
+      role="menu"
+      tabIndex={-1}
+      onKeyDown={handleMenuKeyDown}
+      onMouseMove={handleMenuMouseMove}
+      onBlur={handleMenuBlur}
+    >
+      {children.map((child, index) => {
+        if (menuIndexes[cursor]?.index === index) {
+          let activeChild;
+          if (child.type.toString() === MenuItem.toString()) {
+            activeChild = cloneElement(child, {
+              isActive: true,
+              hasFocus: true,
+              tabIndex: 0,
+              counter: enterCounter,
+            });
+          } else {
+            const buttonElement = (child.props as SubMenuProps).menuRootElement;
+            const cloneButton = cloneElement(buttonElement, {
+              isActive: true,
+              hasFocus: true,
+              tabIndex: 0,
+            });
+            activeChild = cloneElement(child, {
+              menuRootElement: cloneButton,
+              action: cursorAction,
+              counter: enterCounter,
+            });
+          }
+          return <Fragment key={index}>{activeChild}</Fragment>;
+        } else {
+          if (isValidElement(child)) {
+            let inactiveChild;
+            if (child.type.toString() === MenuItem.toString()) {
+              inactiveChild = cloneElement(
+                child as ReactElement<MenuItemProps>,
+                {
+                  isActive: false,
+                  hasFocus: false,
+                  tabIndex: -1,
+                }
+              );
+            } else if (child.type.toString() === SubMenu.toString()) {
+              const buttonElement = (child.props as SubMenuProps)
+                .menuRootElement;
+              const cloneButton = cloneElement(buttonElement, {
+                isActive: false,
+                hasFocus: false,
+                tabIndex: -1,
+              });
+              inactiveChild = cloneElement(
+                child as ReactElement<SubMenuProps>,
+                {
+                  menuRootElement: cloneButton,
+                }
+              );
+            }
+            if (inactiveChild) {
+              return <Fragment key={index}>{inactiveChild}</Fragment>;
+            }
+          }
+          return <Fragment key={index}>{child}</Fragment>;
+        }
+      })}
+    </div>
+  );
+};
+
+export const buildMenuIndexes = (children: MenuProps["children"]) => {
+  const result =
+    Children.map(children, (child, index) => {
+      logger.debug(`building index ${index}`);
+      if (child.type.toString() === MenuItem.toString()) {
+        return { index, id: child.props.id };
+      } else if (child.type.toString() === SubMenu.toString()) {
+        const props = child.props as SubMenuProps;
+        return {
+          index,
+          // using button id for looking up in mouse move event handling
+          id: props.menuRootElement.props.id,
+          length: props.children.length as number,
+        };
+      } else {
+        return null;
+      }
+    }).filter((obj) => !!obj) || [];
+  logger.debug(JSON.stringify(result));
+  return result;
+};
