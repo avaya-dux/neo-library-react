2ca00c2c3e5875a4904f69b948ab8dfdeda79625
Chunguang Yang <chungguangy@avaya.com>
2022-02-11 17:09:50 -0500

    add tests to ScrollHelper

--- src/components/Tab/EventHandlers/Helper.test.jsx
+++ src/components/Tab/EventHandlers/Helper.test.jsx
@@ -1,9 +1,60 @@
-import { moveNextTabToLeftAmount } from "./Helper";
+import {
+  moveNextTabToLeftAmount,
+  movePreviousTabToRightAmount,
+} from "./Helper";
 
-describe(moveNextTabToLeftAmount, () => {
-  it("Right most tab is shown, can not move left any further should return 0", () => {
-    expect(moveNextTabToLeftAmount(300, 400, 100, [100, 100, 100, 100])).toBe(
-      0
-    );
+import {
+  canMovePreviousTabToRight,
+  getPreviousTabToMoveRight,
+  calculateRightMoveAmount,
+  canMoveNextTabToLeft,
+  getNextTabToMoveLeft,
+  calculateLeftMoveAmount,
+} from "./ScrollHelper";
+
+jest.mock("./ScrollHelper");
+
+describe("Tab -> EventHandlers -> Helper", () => {
+  describe(movePreviousTabToRightAmount, () => {
+    it("when canMovePreviousTabToRight is false, should return 0", () => {
+      canMovePreviousTabToRight.mockImplementation(() => false);
+      expect(movePreviousTabToRightAmount(100, 200, 50, [])).toEqual(0);
+      expect(canMovePreviousTabToRight).toBeCalledWith(100, 200, 50);
+    });
+    it("when canMovePreviousTabToRight is true, should call getPreviousTabToMoveRight and calculateRightMoveAmount correctly", () => {
+      const leftOffset = 100;
+      const containerWidth = 200;
+      const viewPortWidth = 500;
+      const retOfGetPreviousTabToMoveRight = [0, 100];
+      const retOfCalculateRightMoveAmount = 100;
+      const tabWidths = [];
+      canMovePreviousTabToRight.mockImplementation(() => true);
+      getPreviousTabToMoveRight.mockImplementation(
+        () => retOfGetPreviousTabToMoveRight
+      );
+      calculateRightMoveAmount.mockImplementation(
+        () => retOfCalculateRightMoveAmount
+      );
+      expect(
+        movePreviousTabToRightAmount(
+          leftOffset,
+          containerWidth,
+          viewPortWidth,
+          []
+        )
+      ).toBe(retOfCalculateRightMoveAmount);
+      expect(getPreviousTabToMoveRight).toBeCalledWith(leftOffset, tabWidths);
+      expect(calculateRightMoveAmount).toBeCalledWith(
+        ...retOfGetPreviousTabToMoveRight,
+        tabWidths
+      );
+    });
+  });
+  describe(moveNextTabToLeftAmount, () => {
+    it("Right most tab is shown, can not move left any further should return 0", () => {
+      expect(moveNextTabToLeftAmount(300, 400, 100, [100, 100, 100, 100])).toBe(
+        0
+      );
+    });
   });
 });
--- src/components/Tab/EventHandlers/ScrollHelper.test.jsx
+++ src/components/Tab/EventHandlers/ScrollHelper.test.jsx
@@ -3,46 +3,142 @@ import {
   getNextTabToMoveLeft,
   calculateLeftMoveAmount,
   calculateRightMoveAmount,
+  canMoveNextTabToLeft,
+  canMovePreviousTabToRight,
 } from "./ScrollHelper";
 
+const sum = (widths) => {
+  return widths.reduce((sum, current) => sum + current);
+};
+describe(canMovePreviousTabToRight, () => {
+  it("when leftOffset === 0 and containerWidth > viewPortWidth, should return false", () => {
+    const leftOffset = 0;
+    const containerWidth = 200;
+    const viewPortWidth = 100;
+    expect(
+      canMovePreviousTabToRight(leftOffset, containerWidth, viewPortWidth)
+    ).toBeFalsy();
+  });
+  it("when leftOffset < 0 and containerWidth > viewPortWidth, should return false", () => {
+    const leftOffset = -10;
+    const containerWidth = 200;
+    const viewPortWidth = 100;
+    expect(
+      canMovePreviousTabToRight(leftOffset, containerWidth, viewPortWidth)
+    ).toBeFalsy();
+  });
+  it("when leftOffset > 0 and containerWidth > viewPortWidth, should return true", () => {
+    const leftOffset = 100;
+    const containerWidth = 200;
+    const viewPortWidth = 100;
+    expect(
+      canMovePreviousTabToRight(leftOffset, containerWidth, viewPortWidth)
+    ).toBeTruthy();
+  });
+  it("when ontainerWidth === viewPortWidth, should return false", () => {
+    const leftOffset = 100;
+    const containerWidth = 100;
+    const viewPortWidth = 100;
+    expect(
+      canMovePreviousTabToRight(leftOffset, containerWidth, viewPortWidth)
+    ).toBeFalsy();
+  });
+  it("when containerWidth < viewPortWidth, should return false", () => {
+    const leftOffset = 0;
+    const containerWidth = 50;
+    const viewPortWidth = 100;
+    expect(
+      canMovePreviousTabToRight(leftOffset, containerWidth, viewPortWidth)
+    ).toBeFalsy();
+  });
+});
 describe(getPreviousTabToMoveRight, () => {
   it("when leftOffset = 0, should return index [-1, 0]", () => {
-    expect(getPreviousTabToMoveRight(0, [100, 100])).toEqual([-1, 0]);
+    const leftOffset = 0;
+    const tab0 = 100;
+    const tab1 = 100;
+    expect(getPreviousTabToMoveRight(leftOffset, [tab0, tab1])).toEqual([
+      -1, 0,
+    ]);
   });
-  it("when leftOffset === first tab width, should return [0, 0]", () => {
-    expect(getPreviousTabToMoveRight(100, [100, 100])).toEqual([0, 0]);
+  it("when leftOffset === tab0, should return [0, 0]", () => {
+    const leftOffset = 100;
+    const tab0 = leftOffset;
+    const tab1 = 100;
+    expect(getPreviousTabToMoveRight(leftOffset, [tab0, tab1])).toEqual([0, 0]);
   });
-  it("when leftOffset is greater than first tab width, should return [1, 50]", () => {
-    expect(getPreviousTabToMoveRight(150, [100, 100])).toEqual([1, 50]);
+  it("when tab0 < leftOffset < tab0 + tab1, should return [1, 150]", () => {
+    const leftOffset = 150;
+    const tab0 = 100;
+    const tab1 = 200;
+    const overshoot = tab0 + tab1 - leftOffset;
+    expect(getPreviousTabToMoveRight(leftOffset, [tab0, tab1])).toEqual([
+      1,
+      overshoot,
+    ]);
   });
-  it("when leftOffset === all tab widths, should return [1, 0]", () => {
-    expect(getPreviousTabToMoveRight(200, [100, 100])).toEqual([1, 0]);
+  it("when leftOffset === all tab widths, should return [last index, 0]", () => {
+    const leftOffset = 300;
+    const tab0 = 100;
+    const tab1 = 100;
+    const tab2 = 100;
+    const overshoot = tab0 + tab1 + tab2 - leftOffset;
+    expect(getPreviousTabToMoveRight(leftOffset, [tab0, tab1, tab2])).toEqual([
+      2,
+      overshoot,
+    ]);
   });
-  it("when leftOffset > all tab widths, should return [1, -100]", () => {
-    expect(getPreviousTabToMoveRight(300, [100, 100])).toEqual([1, -100]);
+  it("when leftOffset > all tab widths, should return [last index, sum - leftOffset]", () => {
+    const leftOffset = 350;
+    const tab0 = 100;
+    const tab1 = 100;
+    const tab2 = 100;
+    const overshoot = tab0 + tab1 + tab2 - leftOffset;
+    expect(getPreviousTabToMoveRight(leftOffset, [tab0, tab1, tab2])).toEqual([
+      2,
+      overshoot,
+    ]);
   });
 });
 
 describe(getNextTabToMoveLeft, () => {
-  it("when leftOffset + viewPortWidth === sum of all tab widths, should return [3, 0]", () => {
-    expect(getNextTabToMoveLeft(100, 100, [50, 50, 50, 50])).toEqual([3, 0]);
+  it("when leftOffset + viewPortWidth === sum of all tab widths, should return [last index, 0]", () => {
+    const leftOffset = 100;
+    const viewPortWidth = 100;
+    const tabWidths = [50, 50, 50, 50];
+    expect(getNextTabToMoveLeft(leftOffset, viewPortWidth, tabWidths)).toEqual([
+      tabWidths.length - 1,
+      0,
+    ]);
   });
-  it("when leftOffset + viewPortWidth >  sum of all tab widths, should return [3, -100]", () => {
-    expect(getNextTabToMoveLeft(100, 200, [50, 50, 50, 50])).toEqual([3, -100]);
-  });
-  it("when leftOffset + viewPortWidth === 160, should return 3", () => {
-    expect(getNextTabToMoveLeft(60, 100, [50, 50, 50, 50])).toEqual([3, 40]);
-  });
-  it("when leftOffset + viewPortWidth === 150, should return 2", () => {
-    expect(getNextTabToMoveLeft(50, 100, [50, 50, 50, 50])).toEqual([2, 0]);
-  });
-  it("when leftOffset + viewPortWidth === 100, should return 1", () => {
-    expect(getNextTabToMoveLeft(0, 100, [50, 50, 50, 50])).toEqual([1, 0]);
+  it("when leftOffset + viewPortWidth >  sum of all tab widths, should return [last index, sum of tabWidths - leftOffset - viewPortWidth]", () => {
+    const leftOffset = 100;
+    const viewPortWidth = 200;
+    const tabWidths = [50, 50, 50, 50];
+
+    expect(getNextTabToMoveLeft(leftOffset, viewPortWidth, tabWidths)).toEqual([
+      tabWidths.length - 1,
+      sum(tabWidths) - leftOffset - viewPortWidth,
+    ]);
   });
 });
 
 describe(calculateLeftMoveAmount, () => {
-  it("when overshoot = 0, should return full tab width", () => {
+  it("when index === last index and overshoot === 0, should return 0", () => {
+    const tabWidths = [50, 60, 70, 80];
+    const overshoot = 0;
+    expect(
+      calculateLeftMoveAmount(tabWidths.length - 1, overshoot, tabWidths)
+    ).toBe(0);
+  });
+  it("when index === last index and overshoot < 0, should return 0", () => {
+    const tabWidths = [50, 60, 70, 80];
+    const overshoot = -10;
+    expect(
+      calculateLeftMoveAmount(tabWidths.length - 1, overshoot, tabWidths)
+    ).toBe(0);
+  });
+  it("when overshoot = 0 and index is not the , should return full tab width", () => {
     const tabWidth = 567;
     expect(calculateLeftMoveAmount(1, 0, [100, tabWidth, 100, 100])).toBe(
       tabWidth
@@ -72,6 +168,10 @@ describe(calculateLeftMoveAmount, () => {
 });
 
 describe(calculateRightMoveAmount, () => {
+  it("when index === -1, should return 0", () => {
+    const index = -1;
+    expect(calculateRightMoveAmount(index, 123, [])).toBe(0);
+  });
   it("when overshoot = 0, should return full tab width", () => {
     const tabWidth = 567;
     expect(calculateRightMoveAmount(1, 0, [100, tabWidth, 100, 100])).toBe(
