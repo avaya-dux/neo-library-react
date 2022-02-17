42b8a7e701a7f66ba7ff4ed53619deb9719190bb
mrazian85 <61242168+mrazian85@users.noreply.github.com>
2022-02-17 08:51:54 -0500

    NEO-805 PR #3 - MultipleSelect additional stories and tests  (#68)
    * get table actions mostly working
    
    * got all the basics working
    
    * allow user to pass custom actions
    
    * simplify custom actions story
    
    * fix hardcoded Pagination id issue
    
    * update code coverage badges
    
    * PR feedback
    
    * fix `Button` bug
    
    class name builder was missing two dependancies
    
    * update Edit and Delete button per discussion with Matt
    
    * fix styling issue per Matt's recommendation
    
    * fix a11y issue for unreadable label
    
    * fix typo
    
    * update test per component update
    
    * update code coverage badges
    
    * update story example per Matt's recommendation
    
    * simplify code
    
    * update code coverage badges
    
    * remove dead code
    
    * per Matt's recommendations, only disable Create btn when `readonly`
    
    * Merging latest changes from main
    
    * Began building Custom Select and Multiple Select using Downshift
    
    * Re-factored Select to simplify implementation and readability
    
    * Began writing tests for Custom Select, removed work on Multiple Select to different branch for subsequent PR
    
    * Added unit and Storybook tests for Custom Select
    
    * Added props for additional Custom Select states
    
    * Continued adding more Stories and tests
    
    * Fixed issue with helper id not displaying correctly
    
    * Added more Story tests
    
    * Tweaked prop to render Select option text and added last few Story tests
    
    * Fixed linter issue
    
    * Removed unused index.ts file
    
    * Added jsdocs content
    
    * Adding updated test badges
    
    * Fixed typo in accessibility handler in Select
    
    * Updated import in Select to avoid circular reference
    
    * Added temporary styles to indicate keyboard interaction
    
    * Ran code formatter
    
    * Began building MultipleSelect
    
    * Pushing preliminary implementation of Multiple Select with children
    
    * Re-factored Multiple Select to use Context API
    
    * Re-factored Select to accept children
    
    * Reverted changes to Select.tsx, added functionality to Multiple Select to use dynamic children
    
    * Changed Multiselect types to improve type safety when mapping over children in Multiselect
    
    * Resolved accessibility issue with checkboxes by adding temporary shim css file to be added to Neo CSS
    
    * Cleaned up MultipleSelect test file
    
    * Resolved errors in SelectTypes.tsx file
    
    * Removed unnecessary Story import in stories file
    
    * Began re-factoring tests, added prop to programmatically control selected values
    
    * Removed unnecessary conditional
    
    * Added additional type security to items const in MultipleSelect
    
    * Continued adding stories and tests
    
    * Continued writing tests for stories
    
    * Continued writing tests for MultipleSelect stories
    
    * Finished adding more tests for MultipleSelect
    
    * Corrected error in Story that prevented helper text from re-appearing on reset
    
    * Fixed type error in event handler for Select
    
    * Removed console log
    
    * Removed another console log
    
    * Removed unnecessary dependecy in useMemo hook in MultipleSelectOption
    
    * Began re-factoring code as per suggestions in PR
    
    * Re-factored test to remove asynchronous logic and make the test less brittle/implementation dependant
    
    * Added additional style for button element when used with spinner
    
    * Removed unnecessary code from Loading Multiple Select story
    
    * Removed unnecessary method
    
    * Added default value for 'values' prop
    
    * Removing unused dep in Stories file
    
    * Re-adding useEffect hook to Loading Multiple Select story
    
    * Reverted code that set a default value for values prop
    
    Co-authored-by: Joe S <jsebast@avaya.com>

--- src/components/Select/MultipleSelect.stories.tsx
+++ src/components/Select/MultipleSelect.stories.tsx
@@ -1,5 +1,9 @@
 import { Meta } from "@storybook/react/types-6-0";
 
+import { useState, useEffect } from "react";
+
+import { Form, Button } from "components";
+
 import { MultipleSelect, MultipleSelectOption } from "./MultipleSelect";
 import { MultipleSelectProps } from "./SelectTypes";
 
@@ -29,11 +33,60 @@ export const DefaultMultipleSelect = () => {
 };
 
 export const RequiredMultipleSelectWithHelperText = () => {
+  const helperTextExample = "Please select one";
+  const [selectedOption, setSelectedOption] = useState<string[]>(["Choice 1"]);
+  const [helperText, setHelperText] = useState(helperTextExample);
+  const [errorList, setErrorList] = useState<string[]>([]);
+
+  const updateSelectedValue = (value: any): any => {
+    setSelectedOption(value);
+    setHelperText(helperText);
+    setErrorList([]);
+  };
+
   return (
-    <MultipleSelect label={label} helperText="This is helper text" required>
-      <MultipleSelectOption>Choice 1</MultipleSelectOption>
-      <MultipleSelectOption>Choice 2</MultipleSelectOption>
-    </MultipleSelect>
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (selectedOption.length > 0) {
+          alert(`you successfully submitted: ${selectedOption.join(", ")}`);
+        }
+      }}
+    >
+      <MultipleSelect
+        values={selectedOption}
+        onSelectedValueChange={updateSelectedValue}
+        label={label}
+        helperText={helperText}
+        errorList={errorList}
+        required
+      >
+        <MultipleSelectOption>Choice 1</MultipleSelectOption>
+        <MultipleSelectOption>Choice 2</MultipleSelectOption>
+      </MultipleSelect>
+      <Button
+        style={{ marginRight: "8px" }}
+        type="submit"
+        onClick={() => {
+          if (selectedOption.length < 1) {
+            setHelperText("");
+            setErrorList(["This is a required field"]);
+          }
+        }}
+      >
+        Submit
+      </Button>
+      <Button
+        type="reset"
+        onClick={() => {
+          setSelectedOption([]);
+          setHelperText(helperTextExample);
+          setErrorList([]);
+        }}
+      >
+        Reset
+      </Button>
+    </Form>
   );
 };
 
@@ -50,9 +103,26 @@ export const DisabledMultipleSelectWithErrorState = () => {
 };
 
 export const LoadingMultipleSelect = () => {
+  const [loading, setLoading] = useState(true);
+  const [options, setOptions] = useState<string[]>([]);
+
+  const fakeLoad = () => {
+    setTimeout(() => {
+      setOptions(["Option 1", "Option 2", "Option 3", "Option 4"]);
+      setLoading(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    fakeLoad();
+    return () => {};
+  }, []);
+
   return (
-    <MultipleSelect label={label} loading>
-      <MultipleSelectOption>Choice 1</MultipleSelectOption>
+    <MultipleSelect label={label} loading={loading}>
+      {options.map((option, index) => (
+        <MultipleSelectOption key={index}>{option}</MultipleSelectOption>
+      ))}
     </MultipleSelect>
   );
 };
@@ -66,5 +136,24 @@ export const MultipleSelectWithWrongChildren = () => {
     <MultipleSelect label="Test Label">
       <p>Test wrong child</p>
     </MultipleSelect>
+  );
+};
+
+export const MoreThanOneMultipleSelect = () => {
+  return (
+    <>
+      <MultipleSelect label="First Multiple Select">
+        <MultipleSelectOption>Option 1</MultipleSelectOption>
+        <MultipleSelectOption>Option 2</MultipleSelectOption>
+        <MultipleSelectOption>Option 3</MultipleSelectOption>
+        <MultipleSelectOption>Option 4</MultipleSelectOption>
+      </MultipleSelect>
+      <MultipleSelect label="Second Multiple Select">
+        <MultipleSelectOption>Option 1</MultipleSelectOption>
+        <MultipleSelectOption disabled>Option 2</MultipleSelectOption>
+        <MultipleSelectOption>Option 3</MultipleSelectOption>
+        <MultipleSelectOption>Option 4</MultipleSelectOption>
+      </MultipleSelect>
+    </>
   );
 };
