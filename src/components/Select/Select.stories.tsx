a177a2d951d80f280b976bb5bda6ffa21ec5907c
Mo Razian <mrazian@avaya.com>
2022-02-18 09:53:12 -0500

    Began re-factoring stories

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -67,104 +67,106 @@ export const DisabledSelect = () => {
   );
 };
 
-// export const RequiredMultipleSelectWithHelperText = () => {
-//   const helperTextExample = "Please select one";
-//   const [selectedOption, setSelectedOption] = useState<string[]>(["Choice 1"]);
-//   const [helperText, setHelperText] = useState(helperTextExample);
-//   const [errorList, setErrorList] = useState<string[]>([]);
+export const RequiredMultipleSelectWithHelperText = () => {
+  const helperTextExample = "Please select one";
+  const [selectedOption, setSelectedOption] = useState<string[]>(["Choice 1"]);
+  const [helperText, setHelperText] = useState(helperTextExample);
+  const [errorList, setErrorList] = useState<string[]>([]);
 
-//   const updateSelectedValue = (value: any): any => {
-//     setSelectedOption(value);
-//     setHelperText(helperText);
-//     setErrorList([]);
-//   };
+  const updateSelectedValue = (value: any): any => {
+    setSelectedOption(value);
+    setHelperText(helperText);
+    setErrorList([]);
+  };
 
-//   return (
-//     <Form
-//       onSubmit={(e) => {
-//         e.preventDefault();
-//         if (selectedOption.length > 0) {
-//           alert(`you successfully submitted: ${selectedOption.join(", ")}`);
-//         }
-//       }}
-//     >
-//       <MultipleSelect
-//         values={selectedOption}
-//         onSelectedValueChange={updateSelectedValue}
-//         label={label}
-//         helperText={helperText}
-//         errorList={errorList}
-//         required
-//       >
-//         <MultipleSelectOption>Choice 1</MultipleSelectOption>
-//         <MultipleSelectOption>Choice 2</MultipleSelectOption>
-//       </MultipleSelect>
-//       <Button
-//         style={{ marginRight: "8px" }}
-//         type="submit"
-//         onClick={() => {
-//           if (selectedOption.length < 1) {
-//             setHelperText("");
-//             setErrorList(["This is a required field"]);
-//           }
-//         }}
-//       >
-//         Submit
-//       </Button>
-//       <Button
-//         type="reset"
-//         onClick={() => {
-//           setSelectedOption([]);
-//           setHelperText(helperTextExample);
-//           setErrorList([]);
-//         }}
-//       >
-//         Reset
-//       </Button>
-//     </Form>
-//   );
-// };
+  return (
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (selectedOption.length > 0) {
+          alert(`you successfully submitted: ${selectedOption.join(", ")}`);
+        }
+      }}
+    >
+      <Select
+        isMultipleSelect
+        values={selectedOption}
+        onSelectedValueChange={updateSelectedValue}
+        label={label}
+        helperText={helperText}
+        errorList={errorList}
+        required
+      >
+        <MultipleSelectOption>Choice 1</MultipleSelectOption>
+        <MultipleSelectOption>Choice 2</MultipleSelectOption>
+      </Select>
+      <Button
+        style={{ marginRight: "8px" }}
+        type="submit"
+        onClick={() => {
+          if (selectedOption.length < 1) {
+            setHelperText("");
+            setErrorList(["This is a required field"]);
+          }
+        }}
+      >
+        Submit
+      </Button>
+      <Button
+        type="reset"
+        onClick={() => {
+          setSelectedOption([]);
+          setHelperText(helperTextExample);
+          setErrorList([]);
+        }}
+      >
+        Reset
+      </Button>
+    </Form>
+  );
+};
 
-// export const DisabledMultipleSelectWithErrorState = () => {
-//   return (
-//     <MultipleSelect
-//       label={label}
-//       errorList={["This is an error message"]}
-//       disabled
-//     >
-//       <MultipleSelectOption>Choice 1</MultipleSelectOption>
-//     </MultipleSelect>
-//   );
-// };
+export const DisabledMultipleSelectWithErrorState = () => {
+  return (
+    <Select
+      isMultipleSelect
+      label={label}
+      errorList={["This is an error message"]}
+      disabled
+    >
+      <MultipleSelectOption>Choice 1</MultipleSelectOption>
+    </Select>
+  );
+};
 
-// export const LoadingMultipleSelect = () => {
-//   const [loading, setLoading] = useState(true);
-//   const [options, setOptions] = useState<string[]>([]);
+export const LoadingMultipleSelect = () => {
+  const [loading, setLoading] = useState(true);
+  const [options, setOptions] = useState<string[]>([]);
 
-//   const fakeLoad = () => {
-//     setTimeout(() => {
-//       setOptions(["Option 1", "Option 2", "Option 3", "Option 4"]);
-//       setLoading(false);
-//     }, 2000);
-//   };
+  const fakeLoad = () => {
+    setTimeout(() => {
+      setOptions(["Option 1", "Option 2", "Option 3", "Option 4"]);
+      setLoading(false);
+    }, 2000);
+  };
 
-//   useEffect(() => {
-//     fakeLoad();
-//     return () => {};
-//   }, []);
+  useEffect(() => {
+    fakeLoad();
+    return () => {};
+  }, []);
 
-//   return (
-//     <MultipleSelect label={label} loading={loading}>
-//       {options.map((option, index) => (
-//         <MultipleSelectOption key={index}>{option}</MultipleSelectOption>
-//       ))}
-//     </MultipleSelect>
-//   );
-// };
+  return (
+    <Select label={label} loading={loading}>
+      {options.map((option, index) => (
+        <MultipleSelectOption key={index}>{option}</MultipleSelectOption>
+      ))}
+    </Select>
+  );
+};
 
-// export const MultipleSelectWithoutChildren = () => {
-//   return <MultipleSelect label="Test Label" />;
-// };
+export const MultipleSelectWithoutChildren = () => {
+  return <Select label="Test Label" />;
+};
 
 export const SelectsWithWrongChildren = () => {
   return (
