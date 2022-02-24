0657ef2db4fa9f931117825821e4829969fbc94d
Joe S <jsebast@avaya.com>
2022-02-23 16:11:22 -0800

    add tests for `Image.tsx`
    couple notes left for improvements that are needed

--- src/components/Image/Image.test.jsx
+++ src/components/Image/Image.test.jsx
@@ -1,47 +1,124 @@
+import { composeStories } from "@storybook/testing-react";
 import { render } from "@testing-library/react";
 import { axe } from "jest-axe";
 
 import { Image } from "./Image";
+import * as ImageStories from "./Image.stories";
 
-describe("ExampleComponent", () => {
+import cat from "./200by300image.jpeg";
+
+const { KittenImage, FallBackAsJSX, FallBackAsUrl } =
+  composeStories(ImageStories);
+
+describe("Image", () => {
   it("fully renders without exploding", () => {
-    const { getByTestId } = render(
-      <Image data-testid="neo-image" alt="test image" role="img" />
-    );
+    const { getByRole } = render(<Image alt="test image" src={cat} />);
 
-    const rootElement = getByTestId("neo-image");
-    expect(rootElement).toBeTruthy();
+    const hiddenImage = getByRole("img", { hidden: true });
+    expect(hiddenImage).toBeTruthy();
+
+    // TODO: this is... not a good test...
+    setTimeout(() => {
+      const displayedImage = getByRole("img", { hidden: false });
+      expect(displayedImage).toBeTruthy();
+    }, 100);
   });
 
   it("passes basic axe compliance", async () => {
-    const { container } = render(
-      <Image data-testid="neo-image" alt="test image" role="img" />
-    );
+    const { container } = render(<Image alt="test image" src={cat} />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
 
-  it("test for className neo is in place", () => {
-    const iconClassName = "neo-img neo-img--fluid";
-    const { getByTestId } = render(
-      <Image data-testid="neo-image" alt="test image" />
-    );
+  it("logs a console warning if the user does not pass `alt` text", () => {
+    const consoleWarnSpy = jest
+      .spyOn(console, "warn")
+      .mockImplementation(() => {});
 
-    const rootElement = getByTestId("neo-image");
-    expect(rootElement).toHaveClass(iconClassName);
+    render(<Image src={cat} />);
+    expect(consoleWarnSpy).toHaveBeenCalledTimes(1);
   });
 
-  it("use a custom className ", () => {
-    const customClassName = "ha-ha css-class-name-test";
-    const { getByTestId } = render(
-      <Image
-        data-testid="neo-image"
-        alt="test image"
-        className={customClassName}
-      />
+  it("calls passed `onError` method if passed and an error is thrown", () => {
+    const onErrorSpy = jest.fn();
+
+    render(
+      <Image alt="test image" src="brokenimage.png" onError={onErrorSpy} />
     );
 
-    const rootElement = getByTestId("neo-image");
-    expect(rootElement).toHaveClass(customClassName);
+    // TODO: this is... not a good test...
+    setTimeout(() => {
+      expect(onErrorSpy).toHaveBeenCalledTimes(1);
+    }, 1000);
+  });
+
+  it("calls passed `onLoad` method if passed and image loads properly", () => {
+    const onLoadSpy = jest.fn();
+
+    render(<Image alt="test image" src="brokenimage.png" onLoad={onLoadSpy} />);
+
+    // TODO: this is... not a good test...
+    setTimeout(() => {
+      expect(onLoadSpy).toHaveBeenCalledTimes(1);
+    }, 1000);
+  });
+
+  describe("storybook tests", () => {
+    describe("KittenImage", () => {
+      let renderResult;
+
+      beforeEach(() => {
+        renderResult = render(<KittenImage />);
+      });
+
+      it("should render ok", () => {
+        const { container } = renderResult;
+        expect(container).not.toBe(null);
+      });
+
+      it("passes basic axe compliance", async () => {
+        const { container } = renderResult;
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
+      });
+    });
+
+    describe("FallBackAsJSX", () => {
+      let renderResult;
+
+      beforeEach(() => {
+        renderResult = render(<FallBackAsJSX />);
+      });
+
+      it("should render ok", () => {
+        const { container } = renderResult;
+        expect(container).not.toBe(null);
+      });
+
+      it("passes basic axe compliance", async () => {
+        const { container } = renderResult;
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
+      });
+    });
+
+    describe("FallBackAsUrl", () => {
+      let renderResult;
+
+      beforeEach(() => {
+        renderResult = render(<FallBackAsUrl />);
+      });
+
+      it("should render ok", () => {
+        const { container } = renderResult;
+        expect(container).not.toBe(null);
+      });
+
+      it("passes basic axe compliance", async () => {
+        const { container } = renderResult;
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
+      });
+    });
   });
 });
