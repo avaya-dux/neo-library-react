767d15b19336c3e845112c790726d9e354999fe5
Joe S <jsebast@avaya.com>
2022-03-14 10:27:01 -0700

    improve Switch stories
    condense most of them to one story, and make the "FormControl" story actually use a form in a reasonable way

--- src/components/Switch/Switch.stories.tsx
+++ src/components/Switch/Switch.stories.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Form } from "components/Form";
 
 import { Switch, SwitchProps } from "./";
+import { Button } from "components/Button";
 
 export default {
   title: "Components/Switch",
@@ -18,37 +19,65 @@ export const Default = () => {
   return (
     <section style={{ width: 200 }}>
       <Switch onChange={(_e, checked) => alert("Checked -> " + checked)}>
-        Alert on Switch toggle
+        Alert on toggle
       </Switch>
 
       <Switch
         checked={checked}
         onChange={(_e, updatedChecked) => setChecked(updatedChecked)}
       >
-        Controlled Checked
+        Controlled Switch
       </Switch>
 
       <Switch defaultChecked>Default Checked</Switch>
 
-      <Switch>Disabled Unchecked</Switch>
-      <Switch defaultChecked>Disabled Checked</Switch>
+      <Switch disabled>Disabled Unchecked</Switch>
+      <Switch disabled defaultChecked>
+        Disabled Checked
+      </Switch>
 
       <Switch multiline>Long label WITH 'multiline' enabled: {longText}</Switch>
-      {/* <Switch label={`Long label _without_ 'multiline' enabled: ${longText}`} >aaaaaaaaaaaaaa</Switch> */}
     </section>
   );
 };
 
 export const FormControl = () => {
+  const [submitted, setSubmitted] = useState(false);
+
   return (
-    <Form>
-      <Switch error value="">
-        Error
+    <Form
+      onSubmit={(e: any) => {
+        e.preventDefault();
+        const tosValue = e.target.elements.ToS.value;
+        setSubmitted(true);
+        alert(`ToS '${tosValue}', form submitted successfully!`);
+      }}
+      style={{ width: 300 }}
+    >
+      <p style={{ paddingBottom: 20 }}>
+        Terms of Service Example. User must accept ToS before being allowed to
+        proceed.
+      </p>
+
+      <Switch required name="ToS" value="accepted">
+        Do you accept the Terms of Service?
       </Switch>
-      <Switch required>Required</Switch>
-      <Switch error required>
-        Required, Error
-      </Switch>
+
+      <section style={{ display: "flex", justifyContent: "space-between" }}>
+        <Button
+          type="reset"
+          variant="secondary"
+          onClick={() => setSubmitted(false)}
+        >
+          Reset
+        </Button>
+
+        <Button type="submit">Submit</Button>
+      </section>
+
+      <p style={{ textAlign: "center" }}>
+        Form Submitted: {submitted ? "TRUE" : "FALSE"}
+      </p>
     </Form>
   );
 };
@@ -58,5 +87,11 @@ export const Template: Story<SwitchProps> = (props: SwitchProps) => (
 );
 
 Template.args = {
+  checked: false,
   children: "Switch Label Text",
+  defaultChecked: true,
+  disabled: false,
+  error: false,
+  multiline: true,
+  required: false,
 };
