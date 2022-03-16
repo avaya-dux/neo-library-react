5a0c38f988c83bb4565aa453061bc68cb2890d1d
Thota <thotaa@avaya.com>
2022-03-16 17:20:46 -0400

    fully functional code

--- src/components/AgentCard/AgentCard.tsx
+++ src/components/AgentCard/AgentCard.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import { AvatarProps } from "components/Avatar";
-import { ReactElement, useRef, useState, useEffect } from "react";
+import { ReactElement } from "react";
 import { Timer } from "./Timer";
 export interface AgentCardProps {
   label: string;
@@ -9,43 +9,7 @@ export interface AgentCardProps {
   avatar?: ReactElement<AvatarProps>;
 }
 
-export const AgentCard = ({
-  label,
-  agentState,
-  imgSrc,
-  avatar,
-}: AgentCardProps) => {
-  // const [agentState, setAgentState] = useState(agentState1);
-  const [count, setCount] = useState(0);
-  const [startStop, setStartStop] = useState(true);
-  const timerIdRef = useRef<NodeJS.Timeout | null | number>(null);
-  const hour: number | string = Math.floor(count / 3600);
-  const minute: number | string = Math.floor((count - hour * 3600) / 60);
-  const seconds: number | string = count - (hour * 3600 + minute * 60);
-
-  useEffect(() => {
-    startHandler();
-    console.log("here i am");
-    // setCount(0);
-    return () => clearInterval(timerIdRef.current as NodeJS.Timeout);
-  }, [agentState]);
-
-  const startHandler = () => {
-    console.log("starthandler");
-    setCount(0);
-    // if (startStop) {
-    if (timerIdRef.current) {
-      clearInterval(timerIdRef.current as NodeJS.Timeout);
-      timerIdRef.current = 0;
-      // return;
-    }
-    timerIdRef.current = setInterval(() => setCount((c) => c + 1), 1000);
-    // setStartStop(!startStop);
-    // } else {
-    // setStartStop(!startStop);
-    // }
-  };
-
+export const AgentCard = ({ label, agentState, avatar }: AgentCardProps) => {
   const initialLetters = (name: string) => {
     if (!name) {
       return "";
@@ -80,7 +44,7 @@ export const AgentCard = ({
             {agentState === "ready" && "Ready"}
             {agentState === "not-ready" && "Not Ready"}
             {agentState === "connected" && "Connected"} {"  "}
-            <Timer count={count} startHandler={startHandler} />
+            <Timer agentState={agentState} />
           </span>
         </div>
         {avatar}
--- src/components/AgentCard/Timer.tsx
+++ src/components/AgentCard/Timer.tsx
@@ -1,19 +1,35 @@
+import { useRef, useState, useEffect } from "react";
 export interface TimerProps {
-  count: number;
-  startHandler?: any;
+  agentState: string;
 }
 
-export const Timer = ({ count, startHandler }: TimerProps) => {
+export const Timer = ({ agentState }: TimerProps) => {
+  const [count, setCount] = useState(0);
+  const timerIdRef = useRef<NodeJS.Timeout | null | number>(null);
   const hour: number | string = Math.floor(count / 3600);
   const minute: number | string = Math.floor((count - hour * 3600) / 60);
   const seconds: number | string = count - (hour * 3600 + minute * 60);
+
+  useEffect(() => {
+    startTimer();
+    return () => clearInterval(timerIdRef.current as NodeJS.Timeout);
+  }, [agentState]);
+
+  const startTimer = () => {
+    console.log("startTimer");
+    setCount(0);
+    if (timerIdRef.current) {
+      clearInterval(timerIdRef.current as NodeJS.Timeout);
+      timerIdRef.current = 0;
+    }
+    timerIdRef.current = setInterval(() => setCount((c) => c + 1), 1000);
+  };
+
   return (
     <>
-      <button onClick={startHandler}>
-        {hour < 10 ? `${"0"}${hour}` : hour}:
-        {minute < 10 ? `${"0"}${minute}` : minute}:
-        {seconds < 10 ? `${"0"}${seconds}` : seconds}
-      </button>
+      {hour < 10 ? `${"0"}${hour}` : hour}:
+      {minute < 10 ? `${"0"}${minute}` : minute}:
+      {seconds < 10 ? `${"0"}${seconds}` : seconds}
     </>
   );
 };
