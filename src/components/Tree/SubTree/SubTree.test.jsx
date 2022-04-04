9d591ccc4ecd3a517df23711c1cf7a8bd5686d95
Joe S <jsebast@avaya.com>
2022-04-04 12:56:39 -0700

    add sub tree tests

--- src/components/Tree/SubTree/SubTree.test.jsx
+++ src/components/Tree/SubTree/SubTree.test.jsx
@@ -1,8 +1,12 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { axe } from "jest-axe";
+
+import { Button } from "components/Button";
 
 import { SubTree } from ".";
 import { Branch } from "../Branch";
+import { Tree } from "../Tree";
 
 describe("SubTree", () => {
   it("fully renders without exploding", () => {
@@ -23,5 +27,60 @@ describe("SubTree", () => {
     );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
+  });
+
+  describe("interactivity", () => {
+    const branchText = "branch";
+    const buttonText = "button";
+    const subTreeText = "sub tree";
+
+    beforeEach(() => {
+      render(
+        <Tree aria-label="testing tree">
+          <SubTree
+            actions={[<Button key="btn-one">{buttonText}</Button>]}
+            edges={[<Branch key="one">{branchText}</Branch>]}
+          >
+            {subTreeText}
+          </SubTree>
+        </Tree>
+      );
+    });
+
+    it("expands and collapses when children are clicked", () => {
+      const subtreeEdges = screen.getByRole("group");
+      const subtreeTitle = screen.getByText(subTreeText);
+
+      expect(subtreeEdges).toBeInTheDocument();
+      expect(subtreeEdges).toHaveClass("neo-display-none");
+      expect(subtreeEdges).toHaveAttribute("aria-expanded", "false");
+
+      expect(subtreeTitle).toBeInTheDocument();
+
+      userEvent.click(subtreeTitle);
+
+      expect(subtreeEdges).not.toHaveClass("neo-display-none");
+      expect(subtreeEdges).toHaveAttribute("aria-expanded", "true");
+
+      userEvent.click(subtreeTitle);
+
+      expect(subtreeEdges).toHaveClass("neo-display-none");
+      expect(subtreeEdges).not.toHaveAttribute("aria-expanded", "true");
+    });
+    it("does _not_ expand/collapse when `actions` are clicked", () => {
+      const subtreeEdges = screen.getByRole("group");
+      const button = screen.getByText(buttonText);
+
+      expect(subtreeEdges).toBeInTheDocument();
+      expect(subtreeEdges).toHaveClass("neo-display-none");
+      expect(subtreeEdges).toHaveAttribute("aria-expanded", "false");
+
+      expect(button).toBeInTheDocument();
+
+      userEvent.click(button);
+
+      expect(subtreeEdges).toHaveClass("neo-display-none");
+      expect(subtreeEdges).toHaveAttribute("aria-expanded", "false");
+    });
   });
 });
