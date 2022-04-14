ab225dc8aceef01737cc19171935b10555b52a76
Chunguang Yang <chungguangy@avaya.com>
2022-04-14 14:23:28 -0400

    update links

--- src/utils/useTimeout.ts
+++ src/utils/useTimeout.ts
@@ -1,8 +1,8 @@
 import { useEffect, useRef } from "react";
 
-// See: https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
 import { useIsomorphicLayoutEffect } from "./useIsomorphicLayoutEffect";
 
+// See: https://usehooks-ts.com/react-hook/use-timeout
 function useTimeout(callback: () => void, delay: number | null) {
   const savedCallback = useRef(callback);
 
