99b4fa0e070a5268b7d561ec3958ea68175dcc30
Enrique Prado <enriquep@avaya.com>
2022-04-26 15:57:45 -0700

    Now using LeftNavigation component in story

--- src/components/LeftNavigation/NavCategory/NavCategory.stories.tsx
+++ src/components/LeftNavigation/NavCategory/NavCategory.stories.tsx
@@ -31,26 +31,22 @@ export const TextOnly: Story<NavCategoryProps> = () => (
 );
 
 export const WithIcons: Story<NavCategoryProps> = () => (
-  <div className="neo-leftnav--wrapper">
-    <nav className="neo-leftnav">
-      <ul className="neo-leftnav__nav">
-        <NavCategory icon="audio-on" label="Collapsed">
-          <LinkItem> First Item </LinkItem>
-          <LinkItem> Second Item </LinkItem>
-          <LinkItem> Third Item </LinkItem>
-          <LinkItem> Fourth Item </LinkItem>
-        </NavCategory>
-        <NavCategory active expanded icon="call" label="Active">
-          <LinkItem> Item 1 </LinkItem>
-          <LinkItem active={true}> Active Item 2 </LinkItem>
-          <LinkItem disabled> Disabled Item </LinkItem>
-          <LinkItem> Item 3</LinkItem>
-        </NavCategory>
-        <NavCategory disabled icon="available" label="Disabled Category">
-          <LinkItem> First Item </LinkItem>
-          <LinkItem> Second Item </LinkItem>
-        </NavCategory>
-      </ul>
-    </nav>
-  </div>
+  <LeftNavigation currentUrl="">
+    <NavCategory icon="audio-on" label="Collapsed">
+      <LinkItem> First Item </LinkItem>
+      <LinkItem> Second Item </LinkItem>
+      <LinkItem> Third Item </LinkItem>
+      <LinkItem> Fourth Item </LinkItem>
+    </NavCategory>
+    <NavCategory active expanded icon="call" label="Active">
+      <LinkItem> Item 1 </LinkItem>
+      <LinkItem active={true}> Active Item 2 </LinkItem>
+      <LinkItem disabled> Disabled Item </LinkItem>
+      <LinkItem> Item 3</LinkItem>
+    </NavCategory>
+    <NavCategory disabled icon="available" label="Disabled Category">
+      <LinkItem> First Item </LinkItem>
+      <LinkItem> Second Item </LinkItem>
+    </NavCategory>
+  </LeftNavigation>
 );
