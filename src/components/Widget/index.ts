8b041c68a3468893fdd419b6c11bc77feb2f6bf4
Chunguang Yang <chungguangy@avaya.com>
2022-04-28 12:53:22 -0400

    prefix exports

--- src/components/Widget/index.ts
+++ src/components/Widget/index.ts
@@ -1,5 +1,10 @@
-export * from "./WidgetTypes";
-export { Header } from "./Header";
-export { Action } from "./Action";
-export { Content } from "./Content";
+export type {
+  HeaderProps as WidgetHeaderProps,
+  ActionProps as WidgetActionProps,
+  ContentProps as WidgetConentProps,
+  WidgetProps,
+} from "./WidgetTypes";
+export { Header as WidgetHeader } from "./Header";
+export { Action as WidgetAction } from "./Action";
+export { Content as WidgetContent } from "./Content";
 export { Widget } from "./Widget";
