3fe00349f014bbd1fb6b25cbfda8f95e60ebffd1
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2022-06-06 12:22:04 -0700

    update deps (#188)
    * update all packages without major updates
    
    also skipping storybook updates as they're causing weird errors (The "id" argument must be of type string)
    
    * resolve a storybook build warning
    
    * update badges
    
    * do minor updates
    
    Co-authored-by: Joe S <jsebast@avaya.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,7 @@ const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app"],
   webpackFinal: async (config) => {
     config.resolve.alias["@avaya/neo-react"] = path.resolve(__dirname, "..");
     config.resolve.plugins = [
