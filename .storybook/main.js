136f0768d040d96bb511d12d1ca9f4ad80b55e6f
Joe S <jsebast@avaya.com>
2022-08-08 14:33:16 -0700

    add missing import for storybook, tweak config for updates

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,12 @@ const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+  ],
+  framework: "@storybook/react",
   webpackFinal: async (config) => {
     config.resolve.alias["@avaya/neo-react"] = path.resolve(__dirname, "..");
     config.resolve.plugins = [
